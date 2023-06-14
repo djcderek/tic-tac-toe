4bff39ee642670806eac9736488c7adff5d5f9a7
Derek Chan <djcderek@gmail.com>
2023-06-13 21:14:38 -0700

    Complete logic for deciding winner

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const gameBoard = (() => {
     let gameBoardArray = [];
     let players = {};
     let currentPlayer = null
+    let splicedArray = []
+    let numRepArray = []
 
     const squares = document.querySelectorAll('.square')
     for (let i=0; i < squares.length; i++) {
@@ -10,7 +12,38 @@ const gameBoard = (() => {
             calculateCurrentTurn()
             updateArray(currentPlayer, Number(squares[i].dataset.index))
             displayArray()
+            createNumArray(currentPlayer)
+            checkIfWon(currentPlayer)
         })
+    }
+
+    const createNumArray = (currentPlayer) => {
+        for (let i = 0; i < 3; i++) {
+            let tempArray = gameBoardArray.slice()
+            splicedArray[i] = tempArray.splice(i*3, 3)
+            numRepArray[i] = splicedArray[i].map((x) => {
+                if (x === currentPlayer.playerType) {
+                    return x = 1
+                } else {
+                    return x = 0
+                }
+            })
+        }
+    }
+
+    const checkIfWon = (currentPlayer) => {
+        let rowCounts = []
+        let colCounts = []
+        for (let i = 0; i < 3; i++) {
+            rowCounts[i] = numRepArray[i].reduce((accumulator, currValue) => accumulator + currValue, 0)
+            colCounts[i] = numRepArray[0][i] + numRepArray[1][i] + numRepArray[2][i]
+            console.log(rowCounts)
+            console.log(colCounts)
+            if (rowCounts[i] >= 3 || colCounts[i] >= 3) {
+                console.log(`${currentPlayer.name} you won!`)
+                break
+            }
+        }
     }
 
     const displayArray = () => {
@@ -50,6 +83,7 @@ const gameBoard = (() => {
 
     return {
         addPlayer: addPlayer,
+        numRepArray: numRepArray
     }
 })()
 
